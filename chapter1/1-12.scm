1cfa55ab74cb4b3efc3b209ada2bc70d6d1014cf
YoungLee <YoungLeeNENU@gmail.com>
2013-03-13 21:23:11 +0800

    some new practices

---
+++ chapter1/1-12.scm
@@ -0,0 +1,6 @@
+;; 计算杨辉三角scheme递归版
+(define (pascal line)
+  (pascal-iter up-1 up-2)
+  (define (pascal-iter up-1 up-2)
+	(define outline 1))
+  )
--- chapter1/1-7.scm
+++ chapter1/1-7.scm
@@ -1,17 +1,21 @@
 ;; 改进的牛顿法求平方根程序
 (define (sqrt x)
+  (define delta 0)
+;; 猜测值的变化率小于一个阈值时
   (define (good-enough? guess x)
 	(define (square x)
 	  (* x x))
-	(< (abs (- (square guess) x)) 0.001))
+	(< (abs (/ delta guess)) 0.00001))
   (define (improve guess x)
 	(define (average x y)
 	  (/ (+ x y) 2))
 	(average guess (/ x guess)))
   (define (sqrt-iter guess x)
+	(define tmp (improve guess x))
+	(set! delta (abs (- tmp guess)))
 	(if (good-enough? guess x)
 		guess
-		(sqrt-iter (improve guess x) x)))
+		(sqrt-iter tmp x)))
   (sqrt-iter 1.0 x))
 
 (begin
