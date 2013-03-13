51e96256d93a26bcc02048445c3e0666ddb7e4ec
YoungLee <YoungLeeNENU@gmail.com>
2013-03-13 22:42:56 +0800

    some changes

--- chapter1/1-12.scm
+++ chapter1/1-12.scm
@@ -1,6 +1,6 @@
 ;; 计算杨辉三角scheme递归版
-(define (pascal line)
-  (pascal-iter up-1 up-2)
-  (define (pascal-iter up-1 up-2)
-	(define outline 1))
-  )
+(define (pascal line id)
+  (if (or (= id 1) (= id line))
+	  1
+	  (+ (pascal (- line 1) id) (pascal (- line 1) (- id 1)))))
+(display (pascal (read) (read)))
