b231c1cd85297bab971ac17398cffd147ec06bed
YoungLee <YoungLeeNENU@gmail.com>
2013-04-21 12:33:50 +0800

    2-4.scm

--- chapter2/2-4.scm
+++ chapter2/2-4.scm
@@ -13,6 +13,7 @@
   ;; car 的 z 参数是 (cons "head" "tail")，(cons "head" "tail") 过程要将参数 m 作用于参数 x 和 y
   ;; 所以 car 实际的操作即把过程 (lambda (p q) p) 作为 m 传递给 z，z 为 (cons "head" "tail")
   ;; m 作用于 "head" 和 "tail" 返回第一个参数，所以返回了 "head"
+  ;; 好晦涩...
   (display (car (cons "head" "tail")))
   (newline)
   (display (cdr (cons "head" "tail")))
