f17aa9e7acd78bff9224c673ad8c747349f5765c
李旸 <youngleemails@gmail.com>
2013-05-11 12:16:19 +0800

    home, no git.

---
+++ chapter2/2-30.scm
@@ -0,0 +1,14 @@
+;; SICP Practice: 2-30
+(define (square-tree items)
+  (map (lambda (sub-tree)
+  	 (if (pair? sub-tree)
+			 (square-tree sub-tree)
+			 ((lambda (x) (* x x)) sub-tree)))
+	   items))
+
+(begin
+  (define sample (list 1
+					   (list 2 (list 3 4) 5)
+					   (list 6 7)))
+  (display (square-tree sample))
+  (newline))
