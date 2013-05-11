ecf42477eecff35bbb159601d59de22755e1ba7d
李旸 <youngleemails@gmail.com>
2013-05-11 11:59:58 +0800

    home, no git.

---
+++ chapter2/2-28.scm
@@ -0,0 +1,15 @@
+;; SICP Practice: 2-28
+(define (fringe items)
+  (if (not (null? items))
+    (if (not (pair? items))
+		  (append (list items))
+		  (append (fringe (car items))
+				  (fringe (cdr items))))
+	  (append '())))
+
+(begin
+  (define x (list (list 1 2) (list 3 4)))
+  (display (fringe x))
+  (newline)
+  (display (fringe (list x x)))
+  (newline))
