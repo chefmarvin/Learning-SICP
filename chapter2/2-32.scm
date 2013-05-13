ffaa190021e750a92e16ae0a9e53db8d26e30196
YoungLee <YoungLeeNENU@gmail.com>
2013-05-13 15:57:09 +0800

    返回全排列

--- chapter2/2-32.scm
+++ chapter2/2-32.scm
@@ -2,10 +2,13 @@
 (define (subsets s)
   (if (null? s)
 	  (list '())
+	  ;; rest 是 (cdr s) 子集的所有组合
 	  (let ((rest (subsets (cdr s))))
-		(append rest (map (lambda () ())
+		;; 这里添加的内容应该能返回含有 (car s) 的所有组合
+		(append rest (map (lambda (x)
+							(append (list (car s)) x))
 						  rest)))))
 
 (begin
-  (display (subsets '(1 2 3)))
+  (display (subsets '(1 2 3 4)))
   (newline))
