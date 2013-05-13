0df4f05c90d7539e81df4187e15735821e627510
YoungLee <YoungLeeNENU@gmail.com>
2013-05-13 16:01:15 +0800

    返回全排列

--- chapter2/2-32.scm
+++ chapter2/2-32.scm
@@ -5,6 +5,7 @@
 	  ;; rest 是 (cdr s) 子集的所有组合
 	  (let ((rest (subsets (cdr s))))
 		;; 这里添加的内容应该能返回含有 (car s) 的所有组合
+		;; 让 rest 中的每个元素都加上 (car s)
 		(append rest (map (lambda (x)
 							(append (list (car s)) x))
 						  rest)))))
