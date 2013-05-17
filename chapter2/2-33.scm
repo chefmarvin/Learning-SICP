9d47da2ccb0dcb74fc2916b45dfd0c51a80be819
YoungLee <YoungLeeNENU@gmail.com>
2013-05-17 09:00:20 +0800

    accumulate

--- chapter2/2-33.scm
+++ chapter2/2-33.scm
@@ -1,4 +1,5 @@
 ;; SICP Practice: 3-33
+;; 累积器
 (define (accumulate op initial sequence)
   (if (null? sequence)
 	  initial
@@ -6,25 +7,24 @@
 		  (accumulate op initial (cdr sequence)))))
 
 (define (map p sequence)
-  (accumulate (lambda (x y) <??>)
+  (accumulate (lambda (x y) (cons (p x) y))
 			  '()
 			  sequence))
 
 (define (append seq1 seq2)
   (accumulate cons
-			  <??>
-			  <??>))
+			  seq2
+			  seq1))
 
 (define (length sequence)
-  (accumulate <??>
+  (accumulate (lambda (x y) (+ y 1))
 			  0
 			  sequence))
 
 (begin
-  (display (map (lambda (x) (* x x)) '(1 2 3)))
+  (display (map (lambda (x) (- x 1)) '(1 2 3)))
   (newline)
   (display (append '(1 2) '(3 4)))
   (newline)
-  (display (length '(1 2 3)))
-  (newline)
-  )
+  (display (length '(1 2 3 4 5)))
+  (newline))
