48ffdbd091050874b0575a44c7f936171ad04736
YoungLee <YoungLeeNENU@gmail.com>
2013-05-23 17:51:53 +0800

    equal?

--- chapter2/2-17.scm
+++ chapter2/2-17.scm
@@ -3,6 +3,7 @@
   (if (null? (cdr sample))
 	  sample
 	  (last-pair (cdr sample))))
+
 (begin
   (display (last-pair '(1 2 3 4)))
   (newline))
--- chapter2/2-6.scm
+++ chapter2/2-6.scm
@@ -1,22 +1,25 @@
-;;; SICP Practice: 2-6
+;; SICP Practice: 2-6
+;; 0 = lambda f. lambda x. x
 (define zero
   (lambda (f)
 	(lambda (x) x)))
 
+;; 可利用代换求值能定义 two 和 one
 (define (add-1 n)
   (lambda (f)
 	(lambda (x) (f ((n f) x)))))
 
+;; 1 = lambda f. lambda x. f x
 (define one
   (lambda (f)
-	(lambda (f) ())))
+	(lambda (x) (f x))))
 
+;; 2 = lambda f. lambda x. f (f x)
 (define two
-  (lambda (f) ()))
+  (lambda (f)
+	(lambda (x) (f (f x)))))
 
+;; 加法：
 (define +
-  (lambda (f) ()))
-
-(begin
-  (display ((lambda (x) (* x x)) 2))
-  (newline))
+  (lambda (f)
+	(lambda (x) ())))
