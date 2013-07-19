157f0e03ae90b68336bfca7ca466ba094855c67f
李旸 <youngleemails@gmail.com>
2013-07-19 21:38:34 +0800

    Create check.py

---
+++ chapter1/check.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+#!/usr/bin/python
+from __future__ import division
+import re
+import os
+import sys
+import subprocess
+
+num = 47
+chapter = " 1-"
+total = range(1, num)
+finished, unfinished = [], []
+
+tail = [".scm", ".org", "-1.scm", "-2.scm"]
+
+for x in total:
+    head = str(x)
+    ls = "ls" + chapter
+    cmd = []
+    flag = False
+    for y in tail:
+        cmd.append(ls + head + y)
+    for z in cmd:
+        token = z[3:]
+        result = subprocess.check_output(z + "; exit 0", 
+                                         stderr = subprocess.STDOUT,
+                                         shell = True)
+        if token == result.strip():
+            finished.append(x)
+            flag = True
+            break
+    if flag == False:
+        unfinished.append(x)
+        
+percentage = len(finished) / len(total)
+
+print "Finished: ", finished
+print "Unfinished: ", unfinished
+print "Completed: " + str(percentage * 100) + "%"
